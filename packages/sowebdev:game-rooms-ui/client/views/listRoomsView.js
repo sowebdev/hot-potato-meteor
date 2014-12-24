813882e6a58f47c4b4826c620e5e2cb6fd0a9355
Sören Ohnmeiss <soren.ohnmeiss@gmail.com>
2014-12-24 08:48:42 +0100

    Added player count on room listing

--- packages/sowebdev:game-rooms-ui/client/views/listRoomsView.js
+++ packages/sowebdev:game-rooms-ui/client/views/listRoomsView.js
@@ -1,6 +1,16 @@
 Template.listRoomsView.helpers({
     gameRooms: function () {
         return GameRooms.rooms.find();
+    },
+    countPlayers: function () {
+        return this.players.length;
+    },
+    formatPlayerCount: function (count) {
+        var countString = count + ' player';
+        if (count > 1) {
+            countString += 's';
+        }
+        return countString;
     }
 });
 
